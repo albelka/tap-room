6a54fa685a87f8cf8b83c1cdd7be7be2e3945afb
Anne Belka and Steve Burton <anne@>
2017-01-18 09:12:25 -0800

    add intial files and edit form

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,76 +4,60 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-    <ul>
-      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit</button></li>
-    </ul>
+    <h1>Portland Taproom</h1>
+    <div *ngFor="let currentKeg of kegs">
+      <h4>{{currentKeg.name}}</h4>
+      <p>{{currentKeg.brand}}</p>
+      <p>\${{currentKeg.price}}.00</p>
+      <p>{{currentKeg.alcoholContent}} abv</p>
+       <button (click)="editKeg(currentKeg)">Edit</button>
+    </div>
     <hr>
-    <div *ngIf="selectedTask">
-      <h3>{{selectedTask.description}}</h3>
-      <p>Task complete? {{selectedTask.done}}</p>
-      <h3>Edit task</h3>
-      <label>Enter task description:</label>
-      <input [(ngModel)]="selectedTask.description">
-      <label> Enter task priority (1-3):</label>
-      <br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High priority)<br>
-      <button (click)="finishedEditing()">Done editing</button>
-      <button (click)="taskComplete()">Task complete</button>
+    <div *ngIf="selectedKeg">
+      <h3>{{selectedKeg.name}}</h3>
+      <p>Pints left: {{selectedKeg.pints}}</p>
+      <h3>Edit keg</h3>
+      <div class="form-group">
+        <label>Edit keg name:</label>
+        <input [(ngModel)]="selectedKeg.name" [ngModelOptions]="{standalone: true}" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Edit brewery:</label>
+        <input [(ngModel)]="selectedKeg.brand" [ngModelOptions]="{standalone: true}" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Edit price:</label>
+        <input [(ngModel)]="selectedKeg.price" [ngModelOptions]="{standalone: true}" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Edit abv:</label>
+        <input [(ngModel)]="selectedKeg.alcoholContent" [ngModelOptions]="{standalone: true}" class="form-control">
+      </div>
+      <button (click)="finishedEditing()" class="btn">Done editing</button>
     </div>
   </div>
   `
 })
 
 export class AppComponent {
-  currentFocus: string = 'Angular Homework';
-  currentTime = new Date();
-  month: number = this.currentTime.getMonth() + 1;
-  day: number = this.currentTime.getDate();
-  year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-    new Task("Finish weekend Angular homework for Epicodus course", 3),
-    new Task("Begin brainstorming possible JavaScript group projects", 2),
-    new Task("Add README file to last few Angular repos on GitHub", 1)
+  kegs: Keg[] = [
+    new Keg("7", "Upright", 7, "6.5%"),
+    new Keg("Sleigher", "Ninkasi", 6, "7%"),
+    new Keg("Polygamy Porter", "Uinta", 6, "3.2%")
   ];
 
-  priorityColor(currentTask) {
-    if (currentTask.priority === 3) {
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
-
   finishedEditing() {
-    this.selectedTask = null;
+    this.selectedKeg = null;
   }
 
-  selectedTask = null;
+  selectedKeg = null;
 
-  editTask(clickedTask) {
-    this.selectedTask = clickedTask;
-  }
-
-  taskComplete() {
-    this.selectedTask.done = true;
-  }
-
-  isDone(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done");
-    } else {
-      alert("This task is not done. Better get to work!");
-    }
+  editKeg(clickedKeg) {
+    this.selectedKeg = clickedKeg;
   }
 }
 
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public priority: number) {}
+export class Keg {
+  public pints: number = 124;
+  constructor(public name: string, public brand: string, public price: number, public alcoholContent: string) {}
 }
